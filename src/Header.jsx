394c9ecfab419203c0581de3932bb1fc118a182d
Qwartzh <paillerlucas@gmail.com>
2024-11-11 11:41:58 +0100

    On continue les tests

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,14 +2,18 @@ import { useState } from "react"
 
 function Header() {
 
-
+    const [color, setColor] = useState("text-blue-500");
+    const changeColor = () => {
+        setColor("text-red-500");
+    }
 
     return (
         <header>
-            <h1 className="text-3xl text-red-500 bg-slate-100 p-5 rounded-md flex  justify-center">
+            <h1 className="text-3xl text-red-500 bg-slate-100 p-5 rounded-md flex  justify-center"
+            onClick={changeColor}>
                 Welcome to React
             </h1>
-            <p className="text-2xl text-blue-500 bg-slate-400 p-5 rounded-md flex justify-center">
+            <p className={`text-2xl ${ color} bg-slate-400 p-5 rounded-md flex justify-center`}>
                 Edit <code>src/App.jsx</code> and save to reload.
             </p>
         </header>
